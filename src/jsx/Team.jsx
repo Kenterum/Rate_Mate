a845c16deef6c2285fbac64a456a89caf860e375
Kenterum <suleymammadov@gmail.com>
2023-12-19 11:11:55 +0400

    Revamp Team Component with Stylish Profiles and Enhanced Layout: - Added 'Card', 'CardFooter', 'Image', and 'Button' components from '@nextui-org/react' for a modern UI design. - Imported team member images and updated the image sources for visual representation. - Implemented a new heading 'Our Team' with increased font size for a prominent display. - Adjusted the grid layout to include up to four columns on larger screens for a responsive design. - Styled individual team member cards with blurred footers, custom images, rounded corners, and shadow effects. - Included 'About Me' buttons in the card footers for additional interactivity and information access. - Enhanced overall aesthetics with new padding, border styles, and text color adjustments.

--- src/jsx/Team.jsx
+++ src/jsx/Team.jsx
@@ -1,7 +1,107 @@
-import React from 'react'
+import React from "react";
+import { Card, CardFooter, Image, Button } from "@nextui-org/react";
+import nijat from "../assets/nijat.png";
+import murad from "../assets/murad.png"
 
 export default function Team() {
   return (
-    <div>Team</div>
-  )
+    <>
+  <h1 className='text-center text-6xl'>Our Team</h1>
+    <div className="px-80  mt-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 p-4">
+      {/* Nijat Ahmadov */}
+      <Card
+        isFooterBlurred
+        radius="lg"
+        className="border-none"
+      >
+        <Image
+          className="object-cover"
+          height={200}
+          src={nijat}
+        />
+        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <p className="text-tiny text-white/80">Nijat Ahmadov</p>
+          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+            About Me
+          </Button>
+        </CardFooter>
+      </Card>
+      {/* Tural Gasimov */}
+      <Card
+        isFooterBlurred
+        radius="lg"
+        className="border-none"
+      >
+        <Image
+          className="object-cover"
+          height={200}
+          src={nijat}
+        />
+        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <p className="text-tiny text-white/80">Tural Gasimov</p>
+          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+            About Me
+          </Button>
+        </CardFooter>
+      </Card>
+      {/* Suleyman Mammadov */}
+      <Card
+        isFooterBlurred
+        radius="lg"
+        className="border-none"
+      >
+        <Image
+          className="object-cover"
+          height={200}
+          src={nijat}
+        />
+
+        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <p className="text-tiny text-white/80">Suleyman Mammadov</p>
+          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+            About Me
+          </Button>
+        </CardFooter>
+      </Card>
+      {/* Murad Muradzada */}
+      <Card
+        isFooterBlurred
+        radius="lg"
+        className="border-none"
+      >
+        <Image
+          className="object-cover h-full"
+          src={murad}
+        />
+
+        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <p className="text-tiny text-white/80">Murad Muradzada</p>
+          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+            About Me
+          </Button>
+        </CardFooter>
+      </Card>
+      {/* Aykhan Ismayilzada */}
+      <Card
+        isFooterBlurred
+        radius="lg"
+        className="border-none"
+      >
+        <Image
+          className="object-cover"
+          height={200}
+          src={nijat}
+        />
+
+        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <p className="text-tiny text-white/80">Aykhan Ismayilzada</p>
+          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+            About Me
+          </Button>
+        </CardFooter>
+      </Card>
+     
+    </div>
+    </>
+  );
 }
