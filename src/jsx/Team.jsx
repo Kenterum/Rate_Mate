4e632ab49a8cd553670ec24ed99069817f44f009
Kenterum <suleymammadov@gmail.com>
2023-12-19 21:53:36 +0400

    Enhance Team Component with Interactive Member Profiles: - Integrated React 'useState' hook for state management of modal visibility and selected team members. - Added 'Modal', 'ModalHeader', and 'ModalBody' components from '@nextui-org/react' for detailed member information display. - Imported custom CSS file for additional styling and layout customization. - Created a 'members' array with detailed information about each team member, including 'about' text. - Implemented a function 'toggleMemberInfo' to handle opening and closing of modal based on member selection. - Added conditional rendering for blurred image effect and information overlay on team member cards. - Implemented a modal feature to display detailed information about each team member when 'About Me' is clicked. - Refactored team member cards to dynamically render from the 'members' array, enhancing maintainability.

--- src/jsx/Team.jsx
+++ src/jsx/Team.jsx
@@ -1,110 +1,90 @@
-import React from "react";
-import { Card, CardFooter, Image, Button } from "@nextui-org/react";
+import React, { useState } from "react";
+import { Card, CardFooter, Image, Button, Modal, ModalHeader, ModalBody } from "@nextui-org/react";
 import nijat from "../assets/nijat.png";
-import murad from "../assets/murad.png"
-import aykhan from "../assets/aykhan.png"
-import tural from "../assets/tural.png"
-import suleyman from "../assets/suleyman.png"
+import murad from "../assets/murad.png";
+import aykhan from "../assets/aykhan.png";
+import tural from "../assets/tural.png";
+import suleyman from "../assets/suleyman.png";
+import "../css/Team.css";
 
 export default function Team() {
+  const [visible, setVisible] = useState(false);
+  const [selectedMember, setSelectedMember] = useState(null);
+  const [blurMemberName, setBlurMemberName] = useState(null);
+
+  const members = [
+    { name: "Nijat Ahmadov", src: nijat, about: "Nijat, önemli bir proje üzerinde çalışıyor..." },
+    { name: "Tural Gasimov", src: tural, about: "Tural, yazılım geliştirme konusunda deneyimli bir mühendis..." },
+    { name: "Suleyman Mammadov", src: suleyman, about: "Suleyman, tasarım ve kullanıcı deneyimi konularında uzmandır..." },
+    { name: "Murad Muradzada", src: murad, about: "Murad, yeni teknolojilere ilgi duyar ve sürekli öğrenir..." },
+    { name: "Aykhan Ismayilzada", src: aykhan, about: "Aykhan, işbirliği ve ekip çalışması konusunda liderdir..." },
+  ];
+
+  const toggleMemberInfo = (member) => {
+    if (blurMemberName === member.name) {
+      setBlurMemberName(null);
+      setSelectedMember(null);
+      setVisible(false);
+    } else {
+      setSelectedMember(member);
+      setBlurMemberName(member.name);
+      setVisible(true);
+    }
+  };
+
+  const closeModal = () => {
+    setVisible(false);
+    setBlurMemberName(null);
+    setSelectedMember(null);
+  };
+
   return (
     <>
-  <h1 className='text-center text-6xl'>Our Team</h1>
-    <div className="px-80  mt-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 p-4">
-      {/* Nijat Ahmadov */}
-      <Card
-        isFooterBlurred
-        radius="lg"
-        className="border-none"
+      <h1 className='text-center text-6xl'>Our Team</h1>
+      <div className="px-80 mt-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 p-4">
+        {members.map((member, index) => (
+          <Card
+            key={index}
+            className="team-card"
+            isFooterBlurred
+            radius="lg"
+          >
+            <Image
+              src={member.src}
+              height={200}
+              className={`team-image ${blurMemberName === member.name ? 'blur-effect' : ''}`}
+            />
+            <CardFooter className="team-footer">
+              <p className="text-tiny text-black/80">{member.name}</p>
+              <Button
+                variant="flat"
+                color="default"
+                radius="lg"
+                size="sm"
+                onClick={() => toggleMemberInfo(member)}
+              >
+                About Me
+              </Button>
+            </CardFooter>
+            <div className={`info-overlay ${blurMemberName === member.name ? 'show' : ''}`}>
+              <p>About this user</p>
+            </div>
+          </Card>
+        ))}
+      </div>
+      <Modal
+        closeButton
+        aria-labelledby="modal-title"
+        open={visible}
+        onClose={closeModal}
       >
-        <Image
-          className="object-cover"
-          height={200}
-          src={nijat}
-        />
-        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-          <p className="text-tiny text-white/80">Nijat Ahmadov</p>
-          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-            About Me
-          </Button>
-        </CardFooter>
-      </Card>
-      {/* Tural Gasimov */}
-      <Card
-        isFooterBlurred
-        radius="lg"
-        className="border-none"
-      >
-        <Image
-          className="object-cover"
-          height={200}
-          src={tural}
-        />
-        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-          <p className="text-tiny text-white/80">Tural Gasimov</p>
-          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-            About Me
-          </Button>
-        </CardFooter>
-      </Card>
-      {/* Suleyman Mammadov */}
-      <Card
-        isFooterBlurred
-        radius="lg"
-        className="border-none"
-      >
-        <Image
-          className="object-cover"
-          height={200}
-          src={suleyman}
-        />
-
-        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-          <p className="text-tiny text-white/80">Suleyman Mammadov</p>
-          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-            About Me
-          </Button>
-        </CardFooter>
-      </Card>
-      {/* Murad Muradzada */}
-      <Card
-        isFooterBlurred
-        radius="lg"
-        className="border-none"
-      >
-        <Image
-          className="object-cover"
-          src={murad}
-        />
-
-        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-          <p className="text-tiny text-white/80">Murad Muradzada</p>
-          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-            About Me
-          </Button>
-        </CardFooter>
-      </Card>
-      {/* Aykhan Ismayilzada */}
-      <Card
-        isFooterBlurred
-        radius="lg"
-        className="border-none"
-      >
-        <Image
-          className="object-cover"
-          height={200}
-          src={aykhan}
-        />
-
-        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-          <p className="text-tiny text-white/80">Aykhan Ismayilzada</p>
-          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
-            About Me
-          </Button>
-        </CardFooter>
-      </Card>
-     
-    </div>
+        <ModalHeader>
+          <p>About {selectedMember?.name}</p>
+        </ModalHeader>
+        <ModalBody>
+          <p>{selectedMember?.about}</p>
+        </ModalBody>
+      </Modal>
     </>
   );
 }
